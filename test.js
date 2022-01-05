eac6f4f3ab4afd616114f9dab811ff1f447e0b12
Titus Wormer <tituswormer@gmail.com>
2022-01-05 15:06:18 +0100

    Update dev-dependencies

--- test.js
+++ test.js
@@ -4,29 +4,31 @@ import rehypeMeta from 'rehype-meta'
 import rehypeInferTitleMeta from './index.js'
 
 test('rehypeInferTitleMeta', async (t) => {
+  let file = await rehype().use(rehypeInferTitleMeta).process('<h1>a</h1>')
+
   t.deepEqual(
-    (await rehype().use(rehypeInferTitleMeta).process('<h1>a</h1>')).data,
+    file.data,
     {meta: {title: 'a'}},
     'should set a title (in `data.meta`)'
   )
 
+  file = await rehype()
+    .use(rehypeInferTitleMeta, {selector: '.x'})
+    .process('<h1>a</h1><p class=x>b</p>')
+
   t.deepEqual(
-    (
-      await rehype()
-        .use(rehypeInferTitleMeta, {selector: '.x'})
-        .process('<h1>a</h1><p class=x>b</p>')
-    ).data,
+    file.data,
     {meta: {title: 'b'}},
     'should support `options.selector`'
   )
 
+  file = await rehype()
+    .use(rehypeInferTitleMeta)
+    .use(rehypeMeta)
+    .process('<h1>a</h1>')
+
   t.equal(
-    String(
-      await rehype()
-        .use(rehypeInferTitleMeta)
-        .use(rehypeMeta)
-        .process('<h1>a</h1>')
-    ),
+    String(file),
     '<html><head>\n<title>a</title>\n</head><body><h1>a</h1></body></html>',
     'should integrate w/ `rehype-meta`'
   )
